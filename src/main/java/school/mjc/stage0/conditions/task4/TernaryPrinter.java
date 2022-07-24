3a4c60be6d4d05c66740da6dc85388459494cab7
Tamara89-M <tamara.muradyan04@gmail.com>
2022-07-24 09:47:04 +0400

    stage0-module4-conditions-task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String bigger = first > second? "first": "second";
+        System.out.println(bigger);
+    }
+
+    public static void main(String[] args) {
+        TernaryPrinter t = new TernaryPrinter();
+        t.printWhichIsBigger(1,12);
     }
 }
